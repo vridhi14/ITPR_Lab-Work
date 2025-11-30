298f5e85a031af74b2e43ad416ea416319e5ebc8
vridhi <rajeevvridhi@gmail.com>
2025-11-30 13:58:00 +0530

    Create GreatestNumber.java

---
+++ method-1/GreatestNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GreatestNumber {
+    // Method to find greatest number
+    static int findGreatest(int a, int b, int c) {
+        if (a >= b && a >= c)
+            return a;
+        else if (b >= a && b >= c)
+            return b;
+        else
+            return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int greatest = findGreatest(a, b, c);
+        System.out.println("The greatest number is: " + greatest);
+    }
+}
